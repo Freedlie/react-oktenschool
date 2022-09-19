1249bfdf2885f13d5b199e50ccc6e75915c2c7b9
Freedlie <kolich.m2003@gmail.com>
2022-09-19 18:03:38 +0300

    react hw3(don't working)

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import {getPosts} from "../../services/api.info.axios";
 
 const User = (props) => {
-    let {user, click, id} = props;
+    let {user, click} = props;
+
+    let [posts, setPosts] = useState([]);
+
+    useEffect(() =>{
+        getPosts(user.id)
+            .then(value => {
+                setPosts(value.data);
+            })
+    }, [user.id])
+
     return (
         <div>
             <p>{user.id} - {user.name} {user.surname}</p>
             <button onClick={
-                click(id)
+                click(posts)
             }>click me</button>
         </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,15 @@
 import React, {useEffect, useState} from 'react';
 
-import {getUsers, getPosts} from "../../services/api.info.axios";
+import {getUsers} from "../../services/api.info.axios";
 import User from "../user/User";
 import Posts from "../posts/Posts";
 
 const Users = () => {
     let [users, setUsers] = useState([]);
-    let [postId, setPostId] = useState(null);
     let [posts, setPosts] = useState([]);
 
-    let click = (id)=>{
-        setPostId(id);
+    let click = (posts)=>{
+        setPosts(posts);
     }
 
     useEffect(()=>{
@@ -20,23 +19,14 @@ const Users = () => {
             })
     },[])
 
-    useEffect(() =>{
-        getPosts(postId)
-            .then(value => {
-                setPosts(value.data);
-            })
-    }, [postId])
-
-
     return (
         <div>
             <div>
                 {users.map((user, index) => (
                     <User
-                     key = {index}
-                     user = {user}
-                     click = {click}
-                     id = {user.id}/>
+                        key = {index}
+                        user = {user}
+                        click = {click}/>
                 ))}
             </div>
             <div>
