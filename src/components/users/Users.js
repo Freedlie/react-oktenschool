31a5e4fc836cc05cfe0163cf01e1ba979be327b7
Freedlie <kolich.m2003@gmail.com>
2022-09-19 13:09:08 +0300

    react hw3

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,18 +1,22 @@
 import React, {useEffect, useState} from 'react';
 
-// import {getUsers} from "../../services/api.info.axios";
+import {getUsers} from "../../services/api.info.axios";
 import User from "../user/User";
 
 const Users = () => {
-    let {users, setUsers} = useState([]);
+    let [users, setUsers] = useState({});
 
     useEffect(()=>{
-        // getUsers()
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(value => (setUsers(value.data)))
-    })
+        getUsers()
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(value => value.json())
+            .then(value => {
+                console.log(value);
+                (setUsers(value.data))
 
+            })
+
+    })
     return (
         <div>
             <div>hello</div>
