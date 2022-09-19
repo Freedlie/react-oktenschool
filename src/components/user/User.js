a900ba71059866b97f23581d797673b431596831
Freedlie <kolich.m2003@gmail.com>
2022-09-19 12:57:05 +0300

    react hw3

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const User = (props) => {
-    let {user} = props
+    let {user} = props;
     return (
         <div>
-            {}
+            <p>{user.id} - {user.name} {user.surname}</p>
         </div>
     );
 };
 
 export default User;
+
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,18 +1,21 @@
 import React, {useEffect, useState} from 'react';
 
-import {getUsers} from "../../services/api.info.axios";
+// import {getUsers} from "../../services/api.info.axios";
 import User from "../user/User";
 
 const Users = () => {
     let {users, setUsers} = useState([]);
 
     useEffect(()=>{
-        getUsers()
+        // getUsers()
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
             .then(value => (setUsers(value.data)))
-    },[])
+    })
 
     return (
         <div>
+            <div>hello</div>
             <div>
                 {users.map((user, index) => (
                     <User
