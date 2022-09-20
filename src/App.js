770e58de6ab01cc29435bcec6d5bb0e24e31d62f
Freedlie <kolich.m2003@gmail.com>
2022-09-20 14:09:37 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import './App.css';
-import Simpsons from "./components/Simpsons/Simpsons";
-import Characters from "./components/task2/Characters/Characters";
 
 function App() {
   return (
       <div>
-          <Simpsons/>
-          <div className="area"></div>
-        <Characters/>
 
       </div>
 
